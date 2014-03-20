0f34f84858eb8003d0ffd75d9cd9785d4fbd5a9a
vadim.kassin <vadim.kassin@vadimkassin.on.kase.kz>
2014-03-20 15:15:20 +0600

    fdf

--- src/com/vkassin/sbornik22/DetailTaskActivity.java
+++ src/com/vkassin/sbornik22/DetailTaskActivity.java
@@ -5,7 +5,9 @@ import java.util.Iterator;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -14,7 +16,10 @@ import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TextView;
 
+
 public class DetailTaskActivity extends Activity {
+
+	private static final String TAG = "Sbornik.DetailTaskActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +31,49 @@ public class DetailTaskActivity extends Activity {
 
 		TextView tv = (TextView) this.findViewById(R.id.DetailTaskTextView);
 		tv.setText(ti.text);
-
+		Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/GothaProReg.otf");
+		tv.setTypeface(tf);
+		
 		TextView title = (TextView) this.findViewById(R.id.DetTaskListTitle01);
 		title.setText("БЭГ / " + Common.curRazdelName + " / ЗАДАЧА № " + ti.getId()); 
 
 //		ImageView imgMy1 = (ImageView) this.findViewById(R.id.imageMy);
 //		imgMy1.setVisibility((ti.my1 == 1) ? View.VISIBLE : View.GONE);
 
-		TextView idd = (TextView) this.findViewById(R.id.DetailNameTextView);
-		idd.setText("" + ti.getId() + " " + ti.name);
-
+		TextView idd = (TextView) this.findViewById(R.id.DetailNumTextView);
+		idd.setText("" + ti.getId());
+		TextView idname = (TextView) this.findViewById(R.id.DetailNameTextView);
+		idname.setText(ti.name);
+		Typeface tff = Typeface.createFromAsset(getAssets(), "fonts/GothaProMed.otf");
+		idd.setTypeface(tff);
+		idname.setTypeface(tff);
+		
+//		Log.i(TAG, "pic = " + ti.pic);
+		
+		if(ti.pic.equals("")) {
+			
+			ImageView imgDiv1 = (ImageView) this.findViewById(R.id.imageDiv1);
+			imgDiv1.setVisibility(View.GONE);
+			ImageView imgDiv2 = (ImageView) this.findViewById(R.id.imageDiv2);
+			imgDiv2.setVisibility(View.GONE);
+			ImageView imgPic = (ImageView) this.findViewById(R.id.imagePicture);
+			imgPic.setVisibility(View.GONE);
+			TextView ptext = (TextView) this.findViewById(R.id.DetailTaskPicTextView);
+			ptext.setVisibility(View.GONE);
+			
+		} else {
+			
+			String uri = "drawable/" + ti.pic;
+			int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+			ImageView imgPic = (ImageView) this.findViewById(R.id.imagePicture);
+			imgPic.setImageResource(imageResource);
+			TextView ptext = (TextView) this.findViewById(R.id.DetailTaskPicTextView);
+			ptext.setText(ti.picsign);
+			Typeface tf1 = Typeface.createFromAsset(getAssets(), "fonts/GothaProReg.otf");
+			ptext.setTypeface(tf1);
+			
+		}
+		
 	}
 
 	public void goToPic(View view) {
